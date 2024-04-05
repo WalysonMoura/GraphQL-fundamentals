8fb250e86597d65adfce41b5d95a549dbf5f2324
{ Walyson } <walysonmoura222@gmail.com>
2024-04-05 01:02:05 +0000

    new

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,51 @@
 import { ApolloServer } from "@apollo/server";
 import gql from "graphql-tag";
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-const typeDefs = gql`
+const typeDefsd = gql`
   type Query {
     Ola: String
   }
 `;
 
-const resolvers = {
+const resolver = {
   Query: {
     Ola: () => "Hello from the Ola resolver!", // Example resolver implementation
   },
 };
 
+const typeDefs = `#graphql
+  #Os comentários em strings GraphQL (como esta) começam com o símbolo hash (#).
+
+  # Este tipo de "Livro" define os campos consultáveis ​​para cada livro em nossa fonte de dados
+  type Book {
+    title: String
+    author: String
+  }
+
+  # O tipo "Consulta" é especial: lista todas as consultas disponíveis que 
+  # clientes podem executar, junto com o tipo de retorno de cada um. Nisso 
+  # caso, a consulta "livros" retorna uma matriz de zero ou mais Livros (definidos acima)
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // Await server.start() before calling fastifyApollo()
